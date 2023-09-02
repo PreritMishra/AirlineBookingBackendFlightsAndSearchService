960f03e06e696071bc72d044e3b5bc087c2faf44
Prerit <preritmishra1002@gmail.com>
2023-09-02 11:21:48 +0530

    Added Airplanes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
 const db= require('./models/index');
+//const { Airplane }= require('./models/index');
+
 
 const setupAndStartServer = async() => {
     //create express object
@@ -18,59 +20,11 @@ const setupAndStartServer = async() => {
     app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`);
 
-        // const airports = await Airport.findAll({
-        //     include: City
-        // });
-        // console.log(airports);
-        // const airports = await City.findAll({
-        //     where: {
-        //         id: 4
-        //     },
-        //     include: [
-        //         {
-        //             model: Airport
-        //         }
-        //     ]
-        // });
-        // console.log(airports)
-        //->>> fetched all the airports in the city with the help of cityID
-        // const cityId = 7; // Replace 4 with the desired city ID
-
-        // const city = await City.findByPk(cityId, {
-        //     include: Airport,
-        // });
-
-        // if (!city) {
-        //     console.log(`City with ID ${cityId} not found.`);
-        // } else {
-        //     const airports = city.Airports; // Assuming the association is named "Airports"
-        //     console.log(airports);
-        // }
-
-        // const city = await City.findOne({
-        //     where:{
-        //         id: 7
-        //     }
-        // });
-        // const airports = await city.getAirports();
-        // /**
-        //  * const newAirport = await Airport.create({
-        //     name: "Prayagraj Airport",
-        //     id:9  //random city id
-        //     //It will create the airport with the id 9 later we can change by below method the id//
-        // });
-        //  */
-        // const newAirport = await Airport.findOne({
-        //     where: {
-        //         id: 7
-        //     }
-        // });
-        // await city.addAirport(newAirport);
-        // console.log(city,airports);
+        
         if(process.env.SYNC_DB){
             db.sequelize.sync({alter:true});
         }
-
+        
     });
 }
 
